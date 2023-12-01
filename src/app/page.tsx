c8af4b972acd3c3406693f15657e3de20fbff22c
hustlerZzZ <rishabpal2000@gmail.com>
2023-12-01 10:10:52 +0530

    Added home layout and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="flex">
+      <div className="left w-1/2">
+        <div>
+          <h1>Lets make a impact in our lives together</h1>
+          <h2>We protect our loved ones ❤️.</h2>
+        </div>
+        <div>
+          <button className="">Check our plans</button>
+        </div>
+      </div>
+      <div className="right w-1/2">
+        <img src="./heart.jpg" alt="heart" className="w-full" />
+      </div>
+    </div>
+  );
 }
